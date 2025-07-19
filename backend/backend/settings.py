3da83c3a4738d949573e332945d06c514cc0165d
Kian <kianjafari660@gmail.com>
2025-07-19 13:22:29 +0330

    :closed_lock_with_key: Add environment secrets

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,20 +11,17 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a**d4$4qrqwrr2*02o&)j79w0kj5*x^hr^6#*h8ew54gg2+*%c"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
