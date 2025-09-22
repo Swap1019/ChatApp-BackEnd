d06a3bb4ee73eb0fd7ecc73352cfe84db0a02fb4
Kian <kianjafari660@gmail.com>
2025-09-22 11:43:05 +0330

    fixing image rendering

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -10,7 +10,7 @@ class Conversation(models.Model):
     members = models.ManyToManyField(User, verbose_name="Members", related_name="chats")
     is_group = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Group creation date")
-    profile = CloudinaryField("chat",null=True,blank=True)
+    profile = CloudinaryField("image",null=True,blank=True)
 
     def __str__(self):
         return self.name
