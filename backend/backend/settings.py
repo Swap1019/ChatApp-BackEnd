08d52917fb08ca8c32a91a3be89153aad5e78a87
Kian <kianjafari660@gmail.com>
2025-09-23 11:54:42 +0330

    setup redis-fixed url

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,7 +81,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis://red-d395cls9c44c73am1au0", 6379)],
+            "hosts": [(os.getenv("REDIS_URL"), 6379)],
         },
     },
 }
